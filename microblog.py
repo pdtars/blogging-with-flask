9f28871be9d3131bdab0edc5c9b356a980efb7da
Paulo de Tarso <zangarmarsh@gmail.com>
2021-12-17 01:15:32 -0300

    add flask shell context

--- microblog.py
+++ microblog.py
@@ -1,1 +1,11 @@
-from app import app
+from app import app, db
+
+from app.models import User, Post
+
+
+#The following function in microblog.py creates a shell context that adds 
+# the database instance and models to the shell session. use flask shell in the cmd, digita depois 
+# db, User ou Post
+@app.shell_context_processor
+def make_shell_context():
+    return {'db':db, 'User':User, 'Post':Post}
